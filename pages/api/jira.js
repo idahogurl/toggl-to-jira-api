903d8dd97f6ab74c88328c6813578fe80c2dd152
Rebecca Vest <olserebe@hotmail.com>
2021-08-31 13:32:00 -0600

    Remove `timespent` from issue filter

--- pages/api/jira.js
+++ pages/api/jira.js
@@ -83,9 +83,7 @@ export async function getWorklogs({ client, entries, author }) {
       entries.map((e) => e.description && e.description.toLowerCase()).filter(Boolean),
     );
     const issueFilter = getIssueFilter(issueKeys);
-    const { issues } = await client.searchJira(`${issueFilter} AND timespent > 0`, {
-      fields: ['key'],
-    });
+    const { issues } = await client.searchJira(issueFilter);
 
     return Promise.all(
       issues.map((i) => client.getIssueWorklogs(i.key.toLowerCase()).then((result) => ({
