0ba8069bc7739594f8dcb80e29be495c3ed1dbb8
Rebecca Vest <olserebe@hotmail.com>
2021-08-31 13:29:27 -0600

    Remove worklogAuthor from query

--- pages/api/jira.js
+++ pages/api/jira.js
@@ -83,12 +83,9 @@ export async function getWorklogs({ client, entries, author }) {
       entries.map((e) => e.description && e.description.toLowerCase()).filter(Boolean),
     );
     const issueFilter = getIssueFilter(issueKeys);
-    const { issues } = await client.searchJira(
-      `worklogAuthor = '${author}' ${issueFilter} AND timespent > 0`,
-      {
-        fields: ['key'],
-      },
-    );
+    const { issues } = await client.searchJira(`${issueFilter} AND timespent > 0`, {
+      fields: ['key'],
+    });
 
     return Promise.all(
       issues.map((i) => client.getIssueWorklogs(i.key.toLowerCase()).then((result) => ({
